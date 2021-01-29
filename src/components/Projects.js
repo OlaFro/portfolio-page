0247a63f8506af3c57e37139cd3db57e5b656f3b
OlaFro <olka.be@gmail.com>
2021-01-29 16:53:02 +0100

    adding title body and icon to the project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,18 +4,33 @@ import {
   StyledProjectsContainer,
   StyledFlex,
   StyledProject,
+  StyledPic,
+  StyledDescription,
+  StyledTitle,
+  StyledBody,
+  StyledIconContainer,
+  StyledCodepen,
 } from "../style/StyledProjects";
 
 export default function Projects() {
   return (
     <div id="projects">
-      <StyledCard height="150vh" color="#F58549" zIndex="3" projects>
+      <StyledCard height="200vh" color="#F58549" zIndex="3" projects>
         <StyledProjectsContainer>
           <StyledH1>projects</StyledH1>
           <StyledFlex>
             <StyledProject>
-              <div style={{ backgroundColor: "white" }}></div>
-              <div style={{ backgroundColor: "black" }}></div>
+              <StyledPic project1 />
+              <StyledDescription>
+                <StyledTitle>Toggle button</StyledTitle>
+                <StyledBody>
+                  This is toggle button that I styled in CSS. Visit my CodePen
+                  to see the project!
+                </StyledBody>
+                <StyledIconContainer>
+                  <StyledCodepen />
+                </StyledIconContainer>
+              </StyledDescription>
             </StyledProject>
             <StyledProject />
             <StyledProject />
--- src/style/StyledProjects.js
+++ src/style/StyledProjects.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import project1 from "../img/project1.png";
+import { Codepen } from "styled-icons/boxicons-logos";
 
 export const StyledProjectsContainer = styled.div`
   display: grid;
-  grid-template-rows: 20% auto;
+  grid-template-rows: 15% auto;
   width: 80%;
   height: 80%;
-  border: 1px solid black;
 `;
 
 export const StyledFlex = styled.div`
@@ -13,14 +14,44 @@ export const StyledFlex = styled.div`
   grid-template-columns: repeat(3, auto);
   grid-template-rows: repeat(2, auto);
   grid-row-gap: 3rem;
-  border: 1px solid blue;
   justify-content: space-evenly;
 `;
 
 export const StyledProject = styled.div`
-  height: 25rem;
+  height: 40rem;
   width: 20rem;
   background-color: grey;
   display: grid;
   grid-template-rows: repeat(2, 50%);
+`;
+
+export const StyledPic = styled.div`
+  background-image: url(${project1});
+  background-position: center;
+  border: 10px solid white;
+`;
+
+export const StyledDescription = styled.div`
+  background-color: ${(props) => props.theme.theme4.about};
+  padding: 20px;
+`;
+
+export const StyledTitle = styled.h3`
+  color: ${(props) => props.theme.theme4.footer};
+  font-family: "Raleway", sans-serif;
+  font-weight: 900;
+`;
+
+export const StyledBody = styled.span`
+  color: ${(props) => props.theme.theme4.footer};
+`;
+
+export const StyledIconContainer = styled.div`
+  padding: 0.5rem 0;
+`;
+
+export const StyledCodepen = styled(Codepen)`
+  color: ${(props) => props.theme.theme4.footer};
+  height: 2rem;
+  width: 2rem;
 `;
