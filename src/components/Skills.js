585205fcd146ed05a4085eccd91483cfaff515ae
OlaFro <olka.be@gmail.com>
2021-02-03 16:39:15 +0100

    chainging skills comp height from vh to rem

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -32,7 +32,7 @@ import {
 export default function Skills() {
   return (
     <div id="skills">
-      <StyledCard height="60vh" color="#F2A65A" zIndex="4" skills>
+      <StyledCard height="40rem" color="#F2A65A" zIndex="4" skills>
         <StyledSkillsContainer>
           <StyledH2>skills</StyledH2>
           <IconStyleWrapper>
