8e1156d0eaf53b54be04f5f80b44c568d26680d4
OlaFro <olka.be@gmail.com>
2021-02-05 10:08:03 +0100

    hiding nav, showing menu icon in responsive

--- src/style/StyledNav.js
+++ src/style/StyledNav.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import { Menu, X } from "styled-icons/feather";
 
 export const StyledNavContainer = styled.nav`
+  width: 100%;
+  height: 5rem;
   position: -webkit-sticky;
   position: sticky;
   top: 0;
   background-color: ${(props) => props.theme.theme4.hero};
-  padding: 1rem 0;
   z-index: 10;
-  box-shadow: 1px 1px 5px black;
+  box-shadow: 0px 0px 5px black;
   display: flex;
   align-items: center;
 `;
@@ -22,13 +23,28 @@ export const StyledNav = styled.nav`
   a {
     text-decoration: none;
   }
+  @media (max-width: 1550px) {
+    display: none;
+  }
+
+  @media (min-width: 1550px) {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export const StyledMenu = styled(Menu)`
   height: 2rem;
-  color: ${(props) => props.theme.theme4.about};
+  /* color: ${(props) => props.theme.theme4.about}; */
+  color: white;
   padding-left: 2rem;
-  display: none;
+
+  @media (max-width: 1550px) {
+    display: block;
+  }
+  @media (min-width: 1550px) {
+    display: none;
+  }
 `;
 
 export const StyledExit = styled(X)`
